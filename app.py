fb3e053e219ea896b557a77c2c9006b2ce2e9dc5
Prakhar <Prakharg823@gmail.com>
2021-04-01 10:49:56 +0530

    streamlit app completed

--- app.py
+++ app.py
@@ -1,14 +1,57 @@
-from flask import Flask, render_template, Response
-from IPython.core.display import HTML
-from main import ner
+import spacy
+from spacy import displacy
+from collections import Counter
+import streamlit as st
+import spacy_streamlit
+import re
+import requests
 
-doc = 'European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices'
 
-app = Flask(__name__)
+def text_search(search_term):
+    # Scraper function
+    S = requests.Session()
+    #link for Wikipedia api for requests
+    URL = "https://en.wikipedia.org/w/api.php"
 
-@app.route('/')
-def hello_world():
-    return 'hello_world'
+    SEARCHPAGE = str(search_term)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "list": "search",
+        "srsearch": SEARCHPAGE
+    }
+
+    R = S.get(url=URL, params=PARAMS)
+    DATA = R.json()
+    return DATA['query']['search'][0]['snippet']
+
+
+
+
+
+keyword = st.text_input('Enter a keyword for search')
+#keyword = str.replace(keyword, ' ', '_')
+
+# use this url for scraping data 
+
+Text = ''
+flag = True
+
+
+# get URL
+Text = str(text_search(str(keyword)))
+
+Text = re.sub('<span class=\"searchmatch\">.*?</span>', '', Text)
+
+#Text.replace("<span class=\"searchmatch\">", "")
+#Text.replace('</span>','')
+
+if Text == '':
+    flag = False
+
+
+if flag:
+    spacy_streamlit.visualize(["en_core_web_sm"], Text)
+else:
+    st.write('Type in a common search term, No results found in previous')
